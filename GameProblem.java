e44717e83639ea95f405668c4664fa648279a65a
Kaitlin Bleich <kbleich@calpoly.edu>
2018-10-31 10:21:29 -0700

    yay

--- GameProblem.java
+++ GameProblem.java
@@ -36,5 +36,56 @@ public class GameProblem {
     private static void game(int n, int m, int[][] A) {
         int[][] S = new int[n][m];                  //table to hold max scores
         char[][] R = new char[n][m];                //aux table to store decisions (d-down, r-right, e-exit)
+
+        for (int i = n-1; i >= 0; i--)
+        {
+           for (int j = m-1; j >= 0; j--)
+           {
+              if (i == n-1 && j == m-1)
+              {
+                 S[i][j] = A[n-1][m-1];
+                 R[i][j] = 'e';
+              }
+              else if (j == m-1)
+              {
+                 if(S[i+1][m-1] > 0)
+                 {
+                    S[i][j] = S[i+1][m-1] + A[i][m-1];
+                    R[i][j] = 'd';
+                 }
+                 else
+                 {
+                    S[i][j] = A[i][m-1];
+                    R[i][j] = 'e';
+                 }
+              }
+              else if (i == n-1)
+              {
+                  if(S[n-1][j+1] > 0)
+                 {
+                    S[i][j] = S[n-1][j+1] + A[n-1][j];
+                    R[i][j] = 'r';
+                 }
+                 else
+                 {
+                    S[i][j] = A[n-1][j];
+                    R[i][j] = 'e';
+                 }
+              }
+              else
+              {
+                 if(S[i+1][j] > S[i][j+1])
+                 {
+                    S[i][j] = S[i+1][j] + A[i][j];
+                    R[i][j] = 'd';
+                 }
+                 else
+                 {
+                    S[i][j] = S[i][j+1] + A[i][j];
+                    R[i][j] = 'e';
+                 }
+              }
+           }
+        }
     }
 }
